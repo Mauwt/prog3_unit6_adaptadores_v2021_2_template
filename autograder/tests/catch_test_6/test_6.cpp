4474108c1501f99cff5cec6c6ea80b144aa72094
rrivas-utec <rrivas@utec.edu.pe>
2021-11-22 17:39:25 -0500

    update test 6

--- autograder/tests/catch_test_6/test_6.cpp
+++ autograder/tests/catch_test_6/test_6.cpp
@@ -5,11 +5,26 @@
 #include "redirect_io.h"
 #include <string>
 #include <stack>
+#include <utility>
 #include "P6.h"
 using namespace std;
 
+namespace test {
+    class student_t {
+        string name;
+        string surname;
+        double grade{};
+    public:
+        student_t() = default;
+        student_t(string name, string surname, double  grade):
+                name(std::move(name)), surname(surname), grade(grade) {}
+        [[nodiscard]] string get_name() const { return name; }
+        [[nodiscard]] string get_surname() const { return surname; }
+        [[nodiscard]] double get_grade() const { return grade; }
+    };
+}
 static void test_6() {
-    stack<alumno_t> s1;
+    stack<test::student_t> s1;
     int n{};
     cin >> n;
     string nombre;
@@ -19,12 +34,18 @@ static void test_6() {
         cin >> nombre;
         cin >> apellido;
         cin >> nota;
-        s1.push(alumno_t{nombre, apellido, nota});
+        s1.push(test::student_t{nombre, apellido, nota});
     }
     string name_tobe_found;
-    getline(cin, name_tobe_found);
-    auto a1 = find_in_stack (s1, [=](auto a) -> bool { return a.get_nombre() == name_tobe_found; });
-    cout << a1.get_nombre() << " " << a1.get_apellido() << " " << a1.get_nota() << endl;
+    cin >> name_tobe_found;
+    auto comp = [=](test::student_t a) -> bool {
+        return a.get_name() == name_tobe_found;
+    };
+    auto a1 = find_in_stack<test::student_t> (s1, comp);
+
+    cout    << a1.get_name() << " "
+            << a1.get_surname() << " "
+            << a1.get_grade() << endl;
 }
 
 TEST_CASE("Question #6") {
